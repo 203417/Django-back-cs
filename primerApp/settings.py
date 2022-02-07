1400fdf366e046e25cc610b23709feea3ecc9974
203417 <203417@ids.upchiapas.edu.mx>
2022-02-07 00:59:50 -0600

    Create component loadImage

--- primerApp/settings.py
+++ primerApp/settings.py
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
     'primerComponente',
     'Login',
     'Register',
+    'loadImage',
 
     #Librerias agregadas al proyecto
     'rest_framework',
@@ -149,3 +150,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+MEDIA_URL = '/assets/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'assets')
